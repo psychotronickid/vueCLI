1be965caacaaedd1c4df90dc949ece34254ba904
gr404_glsan <Gl4dkikh@mail.ru>
2023-03-01 13:43:37 +0700

    create Login.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/home">Home</router-link> |
+    <router-link to="/catalog">Catalog</router-link> |
+    <router-link to="/basket">Basket</router-link> |
+    <router-link to="/orders">Orders</router-link> |
+    <router-link to="/register">Register</router-link> |
+    <router-link to="/login">Login</router-link>
   </nav>
   <router-view/>
 </template>
@@ -12,19 +16,16 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #6c6e69;
 }
-
 nav {
   padding: 30px;
 }
-
 nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: #6c6e69;
 }
-
 nav a.router-link-exact-active {
-  color: #42b983;
+  color: white;
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,36 @@
 import { createStore } from 'vuex'
+import { loginRequest } from "@/utils/api.js";
 
 export default createStore({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+    state: {
+        token: localStorage.getItem('myAppToken') || '',
+    },
+    getters: {
+        isAuthenticated: (state) => !!state.token,
+    },
+    actions: {
+        AUTH_REQUEST: ({ commit }, user) => {
+            return new Promise((resolve, reject) => {
+                loginRequest(user)
+                    .then((token) => {
+                        commit('AUTH_SUCCESS', token);
+                        localStorage.setItem('myAppToken', token);
+                        resolve();
+                    })
+                    .catch(() => {
+                        commit('AUTH_ERROR');
+                        localStorage.removeItem('myAppToken');
+                        reject();
+                    });
+            });
+        }
+    },
+    mutations: {
+        AUTH_SUCCESS: (state, token) => {
+            state.token = token;
+        },
+        AUTH_ERROR: (state) => {
+            state.token = '';
+        },
+    }
 })
